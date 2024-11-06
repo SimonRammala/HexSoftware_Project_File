01752afd07f19461c4480c67d83fac0cb7a4b14d
Simon Rammala <simonrammala456@gmail.com>
2024-11-06 16:38:14 +0200

    Fixed it so that user can ask the number of pages they want to scrap

--- HexSoftware_Web_Scraper.py
+++ HexSoftware_Web_Scraper.py
@@ -5,21 +5,24 @@ import os
 import webbrowser
 
 data = []
+file_path = os.path.join('Scraped_Data', 'Books.html')
 
 def Scrape():
     current_page = 1
-    proceed = True
+    
+    while current_page <= number_of_pages:
 
-    while proceed:
         print("Scraping page: " + str(current_page))
         
         url = f"https://books.toscrape.com/catalogue/page-{current_page}.html"
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
 
+        current_page += 1
+        
         # Check if 404 page
         if soup.title and "404" in soup.title.text:
-            proceed = False
+            break
         else:
             all_books = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
 
@@ -31,48 +34,42 @@ def Scrape():
                     'Stock': book.find('p', class_='instock availability').text.strip()
                 }
                 data.append(item)
-            
-            proceed = False
-            current_page += 1
-
+        
 def save_data():
     if not os.path.exists('Scraped_Data'):
         os.makedirs('Scraped_Data')
     
     # Change directory only if it exists
-    os.chdir('Scraped_Data')
     
-    # Convert data to DataFrame and save as Excel
+    
+    # Convert data to DataFrame and save as html
     df = pd.DataFrame(data)
-    df.to_html('Books.html')
+    df.to_html(file_path)
 
     print("Data saved to 'Scraped_Data\Books.html'")
 
 def open_scraped_data():
-    # Set the path to the file
-    file_path = os.path.join('Scraped_Data', 'Books.html')
-
-    print(os.getcwd())
     
     # Check if the file exists
     if os.path.exists(file_path):
-        # Load the Excel file into a DataFrame
-        df = pd.read_excel(file_path)
+        try:
+            # Open the file in the browser
+            webbrowser.open(f"file://{os.path.abspath(file_path)}")
+        
 
-
-        print("Displaying Scraped_Data:")
-
-        print(df.head())  
-
-        webbrowser.open_new_tab(file_path)
-        return df
+            print("Displaying Scraped Data:")
+            
+        except Exception as e:
+            print("Error reading HTML file:", e)
+            return None
     else:
-        print("File not found. Please make sure 'Books.html' has been generated.")
+        print("File not found. Please make sure 'Books.html' has been generated in the 'Scraped_Data' folder.")
         return None
 
 
-
 # Run the scraping and save functions
+number_of_pages = int(input("Enter the page number to start scraping from: "))
 Scrape()
 save_data()
 open_scraped_data()
+
